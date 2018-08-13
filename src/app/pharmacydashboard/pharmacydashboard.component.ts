911f306a4d2d3f620894d31c463ee8786cf5b74d
meme-neha <neha.keshri@memeinfotech.com>
2018-08-13 12:52:34 +0530

    get Drug List

--- src/app/pharmacydashboard/pharmacydashboard.component.ts
+++ src/app/pharmacydashboard/pharmacydashboard.component.ts
@@ -22,13 +22,16 @@ export class PharmacydashboardComponent implements OnInit {
   medicineFormErrors: any;
   submitted: boolean = false;
   manufactureFormerrors: any //SHOW ERROR,IF INVALID FORM IS SUBMITTED
-  constructor(private formBuilder: FormBuilder, private router: Router, private sharedService: SharedService, private medicineService: MedicineService, private spinner: NgxSpinnerService, private toastr: ToastrService) {
-    //DRUG TYPE FORM ERRORS
+  drugList: any=[];
+  constructor(private formBuilder: FormBuilder, private router: Router, private sharedService: SharedService, private medicineService: MedicineService, private spinner: NgxSpinnerService, private toastr: ToastrService)
+   {
+    
+    /************DRUG TYPE FORM ERRORS***************/
     this.drugtypeFormErrors = {
       type: {},
       description: {},
     };
-    //MEDICINE FORM ERRORS
+    /***********************MEDICINE FORM ERRORS******************/
     this.medicineFormErrors = {
       medicineId: {},
       name: {},
@@ -46,7 +49,7 @@ export class PharmacydashboardComponent implements OnInit {
       substitute: {},
       gstrate: {}
     }
-    //MANUFACTURE FORM ERRORS
+    /*************************MANUFACTURE FORM ERRORS********************/
     this.manufactureFormerrors = {
       gstin: {},
       name: {},
@@ -61,20 +64,46 @@ export class PharmacydashboardComponent implements OnInit {
   }
 
   ngOnInit() {
-    //DRUG TYPE
-    this.drugtypeForm = this.drugform()
+    /***************************DRUG TYPE*****************/
+    this.drugtypeForm = this.Drugform()
 
     this.drugtypeForm.valueChanges.subscribe(() => {
-      this.ondrugFormValuesChanged();
+      this.onDrugFormValuesChanged();
     });
-    //MEDICINE FORM 
+
+    /******************************MEDICINE FORM***********************/
     this.medicineForm = this.createMedicineForm()
 
     this.medicineForm.valueChanges.subscribe(() => {
       this.onMedicineFormValuesChanged();
     })
+  
+  /****************************************MANUFACTURE FORM*******************/
+  this.manuactureForm = this.createManufactureForm()
+
+    this.medicineForm.valueChanges.subscribe(() => {
+      this.onManufactureFormValuesChanged();
+    })
+
+    this.getAllDrug();
   }
-  // IT CATCHES ALL CHANGES IN FORM
+   /********************************* IT CATCHES ALL CHANGES IN DRUG FORM**************************/
+   onDrugFormValuesChanged() {
+    for (const field in this.drugtypeFormErrors) {
+      if (!this.drugtypeFormErrors.hasOwnProperty(field)) {
+        continue;
+      }
+      // Clear previous errors
+      this.drugtypeFormErrors[field] = {};
+      // Get the control
+      const control = this.drugtypeForm.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        this.drugtypeFormErrors[field] = control.errors;
+      }
+    }
+  }
+  /**************************** IT CATCHES ALL CHANGES IN MEDICINE FORM*******************/
   onMedicineFormValuesChanged() {
     for (const field in this.medicineFormErrors) {
       if (!this.medicineFormErrors.hasOwnProperty(field)) {
@@ -90,31 +119,32 @@ export class PharmacydashboardComponent implements OnInit {
       }
     }
   }
-  // IT CATCHES ALL CHANGES IN FORM
-  ondrugFormValuesChanged() {
-    for (const field in this.drugtypeFormErrors) {
-      if (!this.drugtypeFormErrors.hasOwnProperty(field)) {
+  /**********************************IT CATCHES ALL CHANGES IN MASNUFACTURE FORM */
+  onManufactureFormValuesChanged() {
+    for (const field in this.manufactureFormerrors) {
+      if (!this.manufactureFormerrors.hasOwnProperty(field)) {
         continue;
       }
       // Clear previous errors
-      this.drugtypeFormErrors[field] = {};
+      this.manufactureFormerrors[field] = {};
       // Get the control
-      const control = this.drugtypeForm.get(field);
+      const control = this.manuactureForm.get(field);
 
       if (control && control.dirty && !control.valid) {
-        this.drugtypeFormErrors[field] = control.errors;
+        this.manufactureFormerrors[field] = control.errors;
       }
     }
   }
+ 
   // DRUG FORM
-  drugform() {
+  Drugform() {
     return this.formBuilder.group({
       type: ['', Validators.required],
       description: ['', Validators.required]
     });
   }
 
-  // MEDICINE FORM
+  /********************************** MEDICINE FORM********************/
   createMedicineForm() {
     return this.formBuilder.group({
       medicineId: ['', Validators.required],
@@ -134,6 +164,19 @@ export class PharmacydashboardComponent implements OnInit {
       gstrate: ['', Validators.required],
     });
   }
+    /********************************** MANUFACTURE FORM********************/
+    createManufactureForm() {
+      return this.formBuilder.group({
+        gstin: ['', Validators.required],
+        name: ['', Validators.required],
+        contactName: ['', Validators.required],
+        contactNumber: ['', Validators.required],
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        country: ['', Validators.required],
+        zipcode: ['', Validators.required],
+      });
+    }
   //SAVE MEDICINE FORM
   saveMedicineForm() {
     console.log(this.medicineForm.value)
@@ -157,7 +200,6 @@ export class PharmacydashboardComponent implements OnInit {
   }
 
   submitform() {
-    console.log(this.drugtypeForm.value)
     this.submitted = true;
     console.log("drugformvalue", this.drugtypeForm.value);
     $('#myModal').modal('hide');
@@ -171,6 +213,15 @@ export class PharmacydashboardComponent implements OnInit {
       err => {
         console.log(err);
       })
+  }
+  /*****************GET ALL DRUG TYPE****************/
+  getAllDrug(){
+    this.medicineService.getDrugType().subscribe(data=>{
+      console.log(data);
+      let value:any={}
+      value=data;
+      this.drugList=value
+    })
   }
   // SHOW  TOAST NOTIFICTATION,
   showError() {
